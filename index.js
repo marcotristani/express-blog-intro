bfe0624881490c93c4f87bb29686a1359d22bee9
marcotristani <marco.tristani@hotmail.it>
2026-02-05 13:35:57 +0100

    refactoring: riscritto path immagini flat: creata rotta bacheca per far tornare json con oggetto posts

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ const posts = [
   {
     id: 1,
     title: "Ciambellone",
-    img_src: "public\images\ciambellone.jpeg",
+    img_src: "/images/ciambellone.jpeg",
     tags: ["soft", "sweet", "traditional"],
   },
   {
     id: 2,
     title: "Cracker alla Barbabietola",
-    img_src: "public\images\cracker_barbabietola.jpeg",
+    img_src: "/images/cracker_barbabietola.jpeg",
     tags: ["crisp", "savory", "original", "dietary"],
   },
   {
     id: 3,
     title: "Pane Fritto Dolce",
-    img_src: "public\images\pane_fritto_dolce.jpeg",
+    img_src: "/images/pane_fritto_dolce.jpeg",
     tags: ["sweet", "original", "fried"],
   },
   {
     id: 4,
     title: "Pasta alla Barbabietola",
-    img_src: "public\images\pasta_barbabietola.jpeg",
+    img_src: "/images/pasta_barbabietola.jpeg",
     tags: ["first course", "original", "dietary"],
   },
   {
     id: 5,
     title: "Torta Paesana",
-    img_src: "public\images\torta_paesana.jpeg",
+    img_src: "/images/torta_paesana.jpeg",
     tags: ["sweet", "traditional", "soft", "homemade"],
   },
 ];
@@ -40,6 +40,10 @@ index.use(express.static("public"));
 
 index.get("/", (req, res) => {
   res.send("<h1>Server del mio blog</h1>");
+});
+
+index.get("/bacheca", (req, res) => {
+  res.json(posts);
 });
 
 index.listen(port, () => {
