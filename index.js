8396a647f5cfd89404499e59a85cfeef721127d7
marcotristani <marco.tristani@hotmail.it>
2026-02-05 13:24:06 +0100

    flat: creata cartella public con immagini, creato array di oggetti realitivi alle immagini e definito asset statico in index riferito a cartella public

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ const express = require("express");
 const index = express();
 const port = 3000;
 
+//titolo, contenuto, immagine e tags
+const posts = [
+  {
+    id: 1,
+    title: "Ciambellone",
+    img_src: "public\images\ciambellone.jpeg",
+    tags: ["soft", "sweet", "traditional"],
+  },
+  {
+    id: 2,
+    title: "Cracker alla Barbabietola",
+    img_src: "public\images\cracker_barbabietola.jpeg",
+    tags: ["crisp", "savory", "original", "dietary"],
+  },
+  {
+    id: 3,
+    title: "Pane Fritto Dolce",
+    img_src: "public\images\pane_fritto_dolce.jpeg",
+    tags: ["sweet", "original", "fried"],
+  },
+  {
+    id: 4,
+    title: "Pasta alla Barbabietola",
+    img_src: "public\images\pasta_barbabietola.jpeg",
+    tags: ["first course", "original", "dietary"],
+  },
+  {
+    id: 5,
+    title: "Torta Paesana",
+    img_src: "public\images\torta_paesana.jpeg",
+    tags: ["sweet", "traditional", "soft", "homemade"],
+  },
+];
+
+index.use(express.static("public"));
+
 index.get("/", (req, res) => {
   res.send("<h1>Server del mio blog</h1>");
 });
