79e96187ef8a25d8bded5b517386206c5f65ba78
marcotristani <marco.tristani@hotmail.it>
2026-02-05 13:08:39 +0100

    flat: creata rotta get con risposta titolo html e creato listen per vedere funzionamento server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello express ");
+const express = require("express");
+const index = express();
+const port = 3000;
+
+index.get("/", (req, res) => {
+  res.send("<h1>Server del mio blog</h1>");
+});
+
+index.listen(port, () => {
+  console.log(`server funzionante sulla porta ${port}`);
+});
